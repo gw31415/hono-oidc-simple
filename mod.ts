23d57d927fbd4dcafa894f74772440049c9aebf0
gw31415 <gw31415@amas.dev>
2024-12-07 16:54:25 +0900

    fix: version specifiers

--- mod.ts
+++ mod.ts
@@ -1,18 +1,18 @@
-import type { Context } from "npm:hono";
+import type { Context } from "npm:hono@4.6.9";
 import type {
   Handler,
   HandlerResponse,
   Next,
   TypedResponse,
-} from "npm:hono/types";
-import type { StatusCode } from "npm:hono/utils/http-status";
+} from "npm:hono@4.6.9/types";
+import type { StatusCode } from "npm:hono@4.6.9/utils/http-status";
 import {
   createRemoteJWKSet,
   decodeJwt,
   type JWTPayload,
   jwtVerify,
-} from "npm:jose";
-export type { JWTPayload } from "npm:jose";
+} from "npm:jose@5.9.6";
+export type { JWTPayload } from "npm:jose@5.9.6";
 
 /** 認証エラーの種類 */
 export type OIDCError = "OAuthServerError" | "Unauthorized";
@@ -69,9 +69,9 @@ export abstract class Oidc<Issuer extends string | unknown = unknown> {
     c: Context,
     keys:
       | {
-        token: string;
-        payload: JWTPayload;
-      }
+          token: string;
+          payload: JWTPayload;
+        }
       | undefined,
   ): Promise<void>;
 
@@ -286,7 +286,7 @@ export abstract class Oidc<Issuer extends string | unknown = unknown> {
         authUrl.searchParams.append("scope", "openid");
         if (
           metadata.auth_endpoint ===
-            "https://accounts.google.com/o/oauth2/v2/auth"
+          "https://accounts.google.com/o/oauth2/v2/auth"
         ) {
           authUrl.searchParams.append("access_type", "offline");
           authUrl.searchParams.append("prompt", "consent");
